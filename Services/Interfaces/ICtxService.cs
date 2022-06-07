3f6f915f25497cea208063c908f73105af389c74
Steve Canham <stevecanham@outlook.com>
2022-06-07 12:32:50 +0100

    test3

--- Services/Interfaces/ICtxService.cs
+++ Services/Interfaces/ICtxService.cs
@@ -6,7 +6,7 @@ namespace rmsbe.Services.Interfaces;
 public interface IContextService
 {
     // TEST commit - !!!
-    // and again !!!
+    // and again !!! and again!!!
     
     Task<ICollection<Organisation>> GetOrganisations();
     Task<Organisation> GetOrganisation(int id);
