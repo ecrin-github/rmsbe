c3016c71cb98cadc35a3a65c0a347fcaa4379665
Steve Canham <stevecanham@outlook.com>
2022-06-07 12:25:30 +0100

    test commit to 2 repos

--- Services/Interfaces/ICtxService.cs
+++ Services/Interfaces/ICtxService.cs
@@ -1,30 +1,14 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using ContextService.Models.Ctx;
+using rmsbe.SysModels;
+using rmsbe.DbModels;
 
 namespace rmsbe.Services.Interfaces;
 
 public interface IContextService
 {
+    // TEST commit - !!!
+    
     Task<ICollection<Organisation>> GetOrganisations();
     Task<Organisation> GetOrganisation(int id);
     Task<ICollection<Organisation>> GetOrganisationsByName(string name);
-    
-    Task<ICollection<OrgAttribute>> GetOrgAttributes(int org_id);
-    Task<ICollection<OrgLink>> GetOrgLinks(int org_id);
-    Task<ICollection<OrgLocation>> GetOrgLocations(int org_id);
-    Task<ICollection<OrgName>> GetOrgNames(int org_id);
-    Task<ICollection<OrgRelationship>> GetOrgRelationships(int org_id);
-    Task<ICollection<OrgTypeMembership>> GetOrgTypeMemberships(int org_id);
-    
-    Task<ICollection<People>> GetPeople();
-    Task<People> GetPerson(int id);
-    Task<ICollection<PeopleLink>> GetPersonLinks(int person_id);
-    Task<ICollection<PeopleRole>> GetPersonRoles(int person_id);
-    
-    Task<ICollection<GeogEntity>> GetGeogEntities();
-    Task<GeogEntity> GetGeogEntity(int id);
 
-    Task<ICollection<PublishedJournal>> GetPublishedJournals();
-    Task<PublishedJournal> GetPublishedJournal(int id);
 }
