4bfc89dfa057610f36e858d023502d3f021132b1
Steve Canham <stevecanham@outlook.com>
2022-06-09 19:52:55 +0100

    More controller work!

--- Controllers/MDM/MdmHelperApiController.cs
+++ Controllers/MDM/MdmHelperApiController.cs
@@ -5,121 +5,41 @@ using rmsbe.Services.Interfaces;
 
 namespace rmsbe.Controllers;
 
-public class FilteringApiController : BaseApiController
+public class StatisticsApiController : BaseApiController
 {
-    private readonly IDtpRepository _dtpRepository;
-    private readonly IDupRepository _dupRepository;
-
-    public FilteringApiController(
-        IDtpRepository dtpRepository,
-        IDupRepository dupRepository)
-    {
-        _dtpRepository = dtpRepository ?? throw new ArgumentNullException(nameof(dtpRepository));
-        _dupRepository = dupRepository ?? throw new ArgumentNullException(nameof(dupRepository));
-    }
-
     
-    [HttpPost("pagination/dtp")]
-    [SwaggerOperation(Tags = new []{"Pagination"})]
-    public async Task<IActionResult> PaginateStudies(PaginationRequest paginationRequest)
+    private readonly IObjectRepository _objectRepository;
+    private readonly IStudyRepository _studyRepository;
+
+    public StatisticsApiController(
+        IObjectRepository objectRepository,
+        IStudyRepository studyRepository)
     {
-        var data = await _dtpRepository.PaginateDtp(paginationRequest);
-        if (data.Total == 0) return Ok(new ApiResponse<DtpDto>
-        {
-            Total = 0,
-            Data = null,
-            Page = paginationRequest.Page,
-            Size = paginationRequest.Size,
-            StatusCode = NotFound().StatusCode,
-            Messages = new List<string>() {"No DTPs have been found."}
-        });
-        return Ok(new ApiResponse<DtpDto>
-        {
-            Total = data.Total,
-            Data = data.Data,
-            Page = paginationRequest.Page,
-            Size = paginationRequest.Size,
-            StatusCode = Ok().StatusCode,
-            Messages = null
-        });
+        _objectRepository = objectRepository ?? throw new ArgumentNullException(nameof(objectRepository));
+        _studyRepository = studyRepository ?? throw new ArgumentNullException(nameof(studyRepository));
     }
 
-    [HttpPost("pagination/dup")]
-    [SwaggerOperation(Tags = new []{"Pagination"})]
-    public async Task<IActionResult> PaginateObjects(PaginationRequest paginationRequest)
+    [HttpGet("statistics/studies/statistics")]
+    [SwaggerOperation(Tags = new []{"Statistics"})]
+    public async Task<IActionResult> GetTotalStudies()
     {
-        var data = await _dupRepository.PaginateDup(paginationRequest);
-        if (data.Total == 0) return Ok(new ApiResponse<DupDto>
+        return Ok(new StatisticsResponse()
         {
-            Total = 0,
-            Data = null,
-            Page = paginationRequest.Page,
-            Size = paginationRequest.Size,
-            StatusCode = NotFound().StatusCode,
-            Messages = new List<string>() {"No DUPs have been found."}
-        });
-        return Ok(new ApiResponse<DupDto>
-        {
-            Total = data.Total,
-            Data = data.Data,
-            Page = paginationRequest.Page,
-            Size = paginationRequest.Size,
-            StatusCode = Ok().StatusCode,
-            Messages = null
-        });
+            Total = await _studyRepository.GetTotalStudies()
+        });    
     }
 
-    [HttpPost("filter/dtp/by-title")]
-    [SwaggerOperation(Tags = new []{"Filtering - by title"})]
-    public async Task<IActionResult> FilterDtpByTitle(FilteringByTitleRequest filteringByTitleRequest)
+    [HttpGet("statistics/data-objects/statistics")]
+    [SwaggerOperation(Tags = new []{"Statistics"})]
+    public async Task<IActionResult> GetTotalDataObjects()
     {
-        var data = await _dtpRepository.FilterDtpByTitle(filteringByTitleRequest);
-        if (data.Total == 0) return Ok(new ApiResponse<DtpDto>
+        return Ok(new StatisticsResponse()
         {
-            Total = 0,
-            Data = null,
-            Page = filteringByTitleRequest.Page,
-            Size = filteringByTitleRequest.Size,
-            StatusCode = NotFound().StatusCode,
-            Messages = new List<string>() {"No DTPs have been found."}
-        });
-        return Ok(new ApiResponse<DtpDto>
-        {
-            Total = data.Total,
-            Data = data.Data,
-            Page = filteringByTitleRequest.Page,
-            Size = filteringByTitleRequest.Size,
-            StatusCode = Ok().StatusCode,
-            Messages = null
-        });
+            Total = await _objectRepository.GetTotalDataObjects()
+        });    
     }
     
-    [HttpPost("filter/dup/by-title")]
-    [SwaggerOperation(Tags = new []{"Filtering - by title"})]
-    public async Task<IActionResult> FilterDupByTitle(FilteringByTitleRequest filteringByTitleRequest)
-    {
-        var data = await _dupRepository.FilterDupByTitle(filteringByTitleRequest);
-        if (data.Total == 0) return Ok(new ApiResponse<DupDto>
-        {
-            Total = 0,
-            Data = null,
-            Page = filteringByTitleRequest.Page,
-            Size = filteringByTitleRequest.Size,
-            StatusCode = NotFound().StatusCode,
-            Messages = new List<string>() {"No DUPs have been found."}
-        });
-        return Ok(new ApiResponse<DupDto>
-        {
-            Total = data.Total,
-            Data = data.Data,
-            Page = filteringByTitleRequest.Page,
-            Size = filteringByTitleRequest.Size,
-            StatusCode = Ok().StatusCode,
-            Messages = null
-        });
-    }
-
-
+    
     private readonly IObjectRepository _objectRepository;
     private readonly IStudyRepository _studyRepository;
 /*
--- Services/Interfaces/IContextService.cs
+++ Services/Interfaces/IContextService.cs
@@ -1,3 +1,4 @@
+using rmsbe.SysModels;
 
 namespace rmsbe.Services.Interfaces;
 
@@ -25,127 +26,129 @@ public interface IContextService
     Task<ICollection<RepoAccessType>> GetRepoAccessTypes();
     Task<RepoAccessType> GetRepoAccessType(int id);
     
-            Task<ICollection<CompositeHashType>> GetCompositeHashTypes();
-        Task<CompositeHashType> GetCompositeHashType(int id);
+    Task<ICollection<CompositeHashType>> GetCompositeHashTypes();
+    Task<CompositeHashType> GetCompositeHashType(int id);
 
-        Task<ICollection<ContributionType>> GetContributionTypes();
-        Task<ContributionType> GetContributionType(int id);
-        
-        Task<ICollection<DatasetConsentType>> GetDatasetConsentTypes();
-        Task<DatasetConsentType> GetDatasetConsentType(int id);
-        
-        Task<ICollection<DatasetDeidentificationLevel>> GetDatasetDeidentLevels();
-        Task<DatasetDeidentificationLevel> GetDatasetDeidentLevel(int id);
-        
-        Task<ICollection<DatasetRecordkeyType>> GetDatasetRecordkeyTypes();
-        Task<DatasetRecordkeyType> GetDatasetRecordkeyType(int id);
-        
-        Task<ICollection<DateType>> GetDateTypes();
-        Task<DateType> GetDateType(int id);
-        
-        Task<ICollection<DescriptionType>> GetDescriptionTypes();
-        Task<DescriptionType> GetDescriptionType(int id);
-        
-        Task<ICollection<doiStatusType>> GetdoiStatusTypes();
-        Task<doiStatusType> GetdoiStatusType(int id);
-        
-        Task<ICollection<GenderEligibilityType>> GetGenderEligTypes();
-        Task<GenderEligibilityType> GetGenderEligType(int id);
-        
-        Task<ICollection<GeogEntityType>> GetGeogEntityTypes();
-        Task<GeogEntityType> GetGeogEntityType(int id);
-        
-        Task<ICollection<IdentifierType>> GetIdentifierTypes();
-        Task<IdentifierType> GetIdentifierType(int id);
-        
-        Task<ICollection<LanguageCode>> GetLanguageCodes();
-        Task<LanguageCode> GetLanguageCode(string code);
-        
-        Task<ICollection<LanguageUsageType>> GetLangUsageTypes();
-        Task<LanguageUsageType> GetLangUsageType(int id);
-        
-        Task<ICollection<LinkType>> GetLinkTypes();
-        Task<LinkType> GetLinkType(int id);
-        
-        Task<ICollection<ObjectAccessType>> GetObjectAccessTypes();
-        Task<ObjectAccessType> GetObjectAccessType(int id);
-        
-        Task<ICollection<ObjectClass>> GetObjectClasses();
-        Task<ObjectClass> GetObjectClass(int id);
-        
-        Task<ICollection<ObjectFilterType>> GetObjectFilterTypes();
-        Task<ObjectFilterType> GetObjectFilterType(int id);
-        
-        Task<ICollection<ObjectInstanceType>> GetObjectInstanceTypes();
-        Task<ObjectInstanceType> GetObjectInstanceType(int id);
-        
-        Task<ICollection<ObjectRelationshipType>> GetObjectRelationshipTypes();
-        Task<ObjectRelationshipType> GetObjectRelationshipType(int id);
-        
-        Task<ICollection<ObjectType>> GetObjectTypes();
-        Task<ObjectType> GetObjectType(int id);
+    Task<ICollection<ContributionType>> GetContributionTypes();
+    Task<ContributionType> GetContributionType(int id);
+    
+    Task<ICollection<DatasetConsentType>> GetDatasetConsentTypes();
+    Task<DatasetConsentType> GetDatasetConsentType(int id);
+    
+    Task<ICollection<DatasetDeidentificationLevel>> GetDatasetDeidentLevels();
+    Task<DatasetDeidentificationLevel> GetDatasetDeidentLevel(int id);
+    
+    Task<ICollection<DatasetRecordkeyType>> GetDatasetRecordkeyTypes();
+    Task<DatasetRecordkeyType> GetDatasetRecordkeyType(int id);
+    
+    
+    
+    Task<ICollection<DateType>> GetDateTypes();
+    Task<DateType> GetDateType(int id);
+    
+    Task<ICollection<DescriptionType>> GetDescriptionTypes();
+    Task<DescriptionType> GetDescriptionType(int id);
+    
+    Task<ICollection<doiStatusType>> GetdoiStatusTypes();
+    Task<doiStatusType> GetdoiStatusType(int id);
+    
+    Task<ICollection<GenderEligibilityType>> GetGenderEligTypes();
+    Task<GenderEligibilityType> GetGenderEligType(int id);
+    
+    Task<ICollection<GeogEntityType>> GetGeogEntityTypes();
+    Task<GeogEntityType> GetGeogEntityType(int id);
+    
+    Task<ICollection<IdentifierType>> GetIdentifierTypes();
+    Task<IdentifierType> GetIdentifierType(int id);
+    
+    Task<ICollection<LanguageCode>> GetLanguageCodes();
+    Task<LanguageCode> GetLanguageCode(string code);
+    
+    Task<ICollection<LanguageUsageType>> GetLangUsageTypes();
+    Task<LanguageUsageType> GetLangUsageType(int id);
+    
+    Task<ICollection<LinkType>> GetLinkTypes();
+    Task<LinkType> GetLinkType(int id);
+    
+    Task<ICollection<ObjectAccessType>> GetObjectAccessTypes();
+    Task<ObjectAccessType> GetObjectAccessType(int id);
+    
+    Task<ICollection<ObjectClass>> GetObjectClasses();
+    Task<ObjectClass> GetObjectClass(int id);
+    
+    Task<ICollection<ObjectFilterType>> GetObjectFilterTypes();
+    Task<ObjectFilterType> GetObjectFilterType(int id);
+    
+    Task<ICollection<ObjectInstanceType>> GetObjectInstanceTypes();
+    Task<ObjectInstanceType> GetObjectInstanceType(int id);
+    
+    Task<ICollection<ObjectRelationshipType>> GetObjectRelationshipTypes();
+    Task<ObjectRelationshipType> GetObjectRelationshipType(int id);
+    
+    Task<ICollection<ObjectType>> GetObjectTypes();
+    Task<ObjectType> GetObjectType(int id);
 
-        Task<ICollection<OrgAttributeDatatype>> GetOrgAttributeDatatypes();
-        Task<OrgAttributeDatatype> GetOrgAttributeDatatype(int id);
+    Task<ICollection<OrgAttributeDatatype>> GetOrgAttributeDatatypes();
+    Task<OrgAttributeDatatype> GetOrgAttributeDatatype(int id);
 
-        Task<ICollection<OrgAttributeType>> GetOrgAttributeTypes();
-        Task<OrgAttributeType> GetOrgAttributeType(int id);
+    Task<ICollection<OrgAttributeType>> GetOrgAttributeTypes();
+    Task<OrgAttributeType> GetOrgAttributeType(int id);
 
-        Task<ICollection<OrgClass>> GetOrgClasses();
-        Task<OrgClass> GetOrgClass(int id);
+    Task<ICollection<OrgClass>> GetOrgClasses();
+    Task<OrgClass> GetOrgClass(int id);
 
-        Task<ICollection<OrgLinkType>> GetOrgLinkTypes();
-        Task<OrgLinkType> GetOrgLinkType(int id);
+    Task<ICollection<OrgLinkType>> GetOrgLinkTypes();
+    Task<OrgLinkType> GetOrgLinkType(int id);
 
-        Task<ICollection<OrgNameQualifierType>> GetOrgNameQualifierTypes();
-        Task<OrgNameQualifierType> GetOrgNameQualifierType(int id);
+    Task<ICollection<OrgNameQualifierType>> GetOrgNameQualifierTypes();
+    Task<OrgNameQualifierType> GetOrgNameQualifierType(int id);
 
-        Task<ICollection<OrgRelationshipType>> GetOrgRelationshipTypes();
-        Task<OrgRelationshipType> GetOrgRelationshipType(int id);
+    Task<ICollection<OrgRelationshipType>> GetOrgRelationshipTypes();
+    Task<OrgRelationshipType> GetOrgRelationshipType(int id);
 
-        Task<ICollection<OrgType>> GetOrgTypes();
-        Task<OrgType> GetOrgType(int id);
-        
-        Task<ICollection<ResourceType>> GetResourceTypes();
-        Task<ResourceType> GetResourceType(int id);
-        
-        Task<ICollection<RmsUserType>> GetRmsUserTypes();
-        Task<RmsUserType> GetRmsUserType(int id);
-        
-        Task<ICollection<RoleClass>> GetRoleClasses();
-        Task<RoleClass> GetRoleClass(int id);
-        
-        Task<ICollection<RoleType>> GetRoleTypes();
-        Task<RoleType> GetRoleType(int id);
-        
-        Task<ICollection<SizeUnit>> GetSizeUnits();
-        Task<SizeUnit> GetSizeUnit(int id);
-        
-        Task<ICollection<StudyFeatureCategory>> GetStudyFeatureCategories();
-        Task<StudyFeatureCategory> GetStudyFeatureCategory(int id);
-        
-        Task<ICollection<StudyFeatureType>> GetStudyFeatureTypes();
-        Task<StudyFeatureType> GetStudyFeatureType(int id);
-        
-        Task<ICollection<StudyRelationshipType>> GetStudyRelationshipTypes();
-        Task<StudyRelationshipType> GetStudyRelationshipType(int id);
-        
-        Task<ICollection<StudyStatus>> GetStudyStatuses();
-        Task<StudyStatus> GetStudyStatus(int id);
-        
-        Task<ICollection<StudyType>> GetStudyTypes();
-        Task<StudyType> GetStudyType(int id);
-        
-        Task<ICollection<TimeUnit>> GetTimeUnits();
-        Task<TimeUnit> GetTimeUnit(int id);
-        
-        Task<ICollection<TitleType>> GetTitlesTypes();
-        Task<TitleType> GetTitleType(int id);
-        
-        Task<ICollection<TopicType>> GetTopicTypes();
-        Task<TopicType> GetTopicType(int id);
+    Task<ICollection<OrgType>> GetOrgTypes();
+    Task<OrgType> GetOrgType(int id);
+    
+    Task<ICollection<ResourceType>> GetResourceTypes();
+    Task<ResourceType> GetResourceType(int id);
+    
+    Task<ICollection<RmsUserType>> GetRmsUserTypes();
+    Task<RmsUserType> GetRmsUserType(int id);
+    
+    Task<ICollection<RoleClass>> GetRoleClasses();
+    Task<RoleClass> GetRoleClass(int id);
+    
+    Task<ICollection<RoleType>> GetRoleTypes();
+    Task<RoleType> GetRoleType(int id);
+    
+    Task<ICollection<SizeUnit>> GetSizeUnits();
+    Task<SizeUnit> GetSizeUnit(int id);
+    
+    Task<ICollection<StudyFeatureCategory>> GetStudyFeatureCategories();
+    Task<StudyFeatureCategory> GetStudyFeatureCategory(int id);
+    
+    Task<ICollection<StudyFeatureType>> GetStudyFeatureTypes();
+    Task<StudyFeatureType> GetStudyFeatureType(int id);
+    
+    Task<ICollection<StudyRelationshipType>> GetStudyRelationshipTypes();
+    Task<StudyRelationshipType> GetStudyRelationshipType(int id);
+    
+    Task<ICollection<StudyStatus>> GetStudyStatuses();
+    Task<StudyStatus> GetStudyStatus(int id);
+    
+    Task<ICollection<StudyType>> GetStudyTypes();
+    Task<StudyType> GetStudyType(int id);
+    
+    Task<ICollection<TimeUnit>> GetTimeUnits();
+    Task<TimeUnit> GetTimeUnit(int id);
+    
+    Task<ICollection<TitleType>> GetTitlesTypes();
+    Task<TitleType> GetTitleType(int id);
+    
+    Task<ICollection<TopicType>> GetTopicTypes();
+    Task<TopicType> GetTopicType(int id);
 
-        Task<ICollection<TopicVocabulary>> GetTopicVocabularies();
-        Task<TopicVocabulary> GetTopicVocabulary(int id);
+    Task<ICollection<TopicVocabulary>> GetTopicVocabularies();
+    Task<TopicVocabulary> GetTopicVocabulary(int id);
 }
 
