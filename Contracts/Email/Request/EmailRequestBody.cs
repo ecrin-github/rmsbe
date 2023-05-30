99483983874b25784da8b2b672114d5c468a9a9b
Sergei Gorianin <frequenteen@gmail.com>
2023-05-30 17:35:43 +0400

    Add subject param

--- Contracts/Email/Request/EmailRequestBody.cs
+++ Contracts/Email/Request/EmailRequestBody.cs
@@ -3,6 +3,8 @@ namespace rmsbe.Contracts.Email.Request;
 public class EmailRequestBody
 {
     public IList<string> To { get; set; }
-    
-    public string Text { get; set; }
+
+    public string Subject { get; set; } = string.Empty;
+
+    public string Text { get; set; } = string.Empty;
 }
--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -23,7 +23,7 @@ public class EmailService : IEmailService
         var email = new MimeMessage();
         email.From.Add(MailboxAddress.Parse(SMTPConfigs.SenderEmail));
         email.To.AddRange(recipients);
-        email.Subject = SMTPConfigs.SubjectText;
+        email.Subject = !string.IsNullOrWhiteSpace(emailRequestBody.Subject) ? emailRequestBody.Subject : SMTPConfigs.SubjectText;
         email.Body = new TextPart(TextFormat.Plain) { Text = emailRequestBody.Text };
 
         // send email
